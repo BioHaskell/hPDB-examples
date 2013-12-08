5de174257edd9cd8865bba00d76e47d41ee25563
Michal J. Gajda <mjgajda@gmail.com>
2013-12-08 19:40:51 +0100

    Use Bio.PDB.Structure.Neighbours in StericClashCheck.
    Requires hPDB >= 1.1.

--- examples/StericClashCheck.hs
+++ examples/StericClashCheck.hs
@@ -2,29 +2,20 @@
 
 module Main where
 
-import qualified System.Environment         as Env(getArgs)
-import qualified Data.ByteString.Char8      as BS
-import qualified Data.Octree                as Oct
-import           Bio.PDB                    as PDB
-import qualified Bio.PDB.Structure.Elements as PDB(vanDerWaalsRadius)
+import qualified System.Environment           as Env(getArgs)
+import qualified Data.ByteString.Char8        as BS
+import qualified Data.Octree                  as Oct
+import           Bio.PDB                      as PDB
+import qualified Bio.PDB.Structure.Elements   as PDB(vanDerWaalsRadius)
+import qualified Bio.PDB.Structure.Neighbours as PDB
 
 -- TODO: way to hand over Atom object along with its Residue, Chain and Model
 -- TODO: make it so that Show gives a "full_id" string by default, e.g. Model, chain, residue id, atom name.
 clashCheck s1 s2 = filter (/= []) . Prelude.map clashes $ itfoldr (:) [] s2
   where
-    clashes (at :: PDB.Atom) = Oct.withinRange ot (radius + maxRadius) (PDB.coord at)
+    clashes (at :: PDB.Atom) = PDB.findInRadius (PDB.makeOctree s1) (radius + maxRadius) (PDB.coord at)
       where
         radius :: Double = realToFrac $ PDB.vanDerWaalsRadius . PDB.element $ at
-    ot :: Oct.Octree (Int, Double)
-    ot = makeOctree s1
-
-extract :: PDB.Atom -> (Oct.Vector3, (Int, Double))
-extract (PDB.Atom { coord    = cvec,
-                    atSerial = ser ,
-                    element  = elt }) = (cvec,
-                                         (ser, realToFrac $ PDB.vanDerWaalsRadius elt))
-
-makeOctree structure = Oct.fromList . Prelude.map extract . itfoldr (:) [] $ structure
 
 -- Bio.PDB.Structure.Elements should export max bound for vdw etc.
 -- or a list of known element codes.
@@ -37,9 +28,9 @@ main = do [input1, input2] <- Env.getArgs
           Just structure2 <- PDB.parse input2
           print $ clashCheck structure1 structure2
           print "Depths:"
-          print . Oct.depth . makeOctree $ structure1
-          print . Oct.depth . makeOctree $ structure2
+          print . Oct.depth . PDB.makeOctree $ structure1
+          print . Oct.depth . PDB.makeOctree $ structure2
           print "Sizes:"
-          print . size      . makeOctree $ structure1
-          print . size      . makeOctree $ structure2
+          print . size      . PDB.makeOctree $ structure1
+          print . size      . PDB.makeOctree $ structure2
 
